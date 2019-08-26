2486a78ecf81b8b40734d94f362aa4ba2ea45286
Victor Lu <luzhongping@niwodai.net>
2019-08-26 22:41:42 +0800

    feat(api): make api response slack command

--- app/router.js
+++ app/router.js
@@ -4,10 +4,16 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
-  router.get('/', controller.home.index);
+    const { router, controller } = app;
+    router.get('/', controller.home.index);
 
-  router.post('/api/praise', ctx => {
-    ctx.body = ctx.request.body;
-  });
+    router.post('/api/praise', ctx => {
+        ctx.body = {
+            text: 'Your praise is received!',
+            attachments: Object.keys(ctx.query)
+                .map(key => `${key}: ${ctx.query[key]}`)
+                .map(text => ({ text })),
+        };
+        ctx.set('Content-Type', 'application/json');
+    });
 };
